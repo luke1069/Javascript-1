1480ce7d2893491e9274fa42e90364dd3c97b41e
新井峻 <luke1069onega14mas@gmail.com>
2021-11-02 16:43:41 +0900

    initial_commit

---
+++ practice1/app.js
@@ -0,0 +1,40 @@
+// let hello = 'Hello World!';
+// alert(hello);
+
+// alert(4+3);
+// alert(10/2);
+
+// let str1 = 'Hello';
+// let str2 = 'World!';
+// alert(str1 + str2);
+
+// let orange = 100;
+// let apple = 100;
+
+// if(orange<apple){
+//   alert('みかんの値段がリンゴより安い');
+// }else if(orange == apple){
+//   alert('みかんとリンゴは同じ値段');
+// }else{
+//   alert('みかんの値段がリンゴより高い')
+// }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while( num < max ){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛けて' + max + 'を超えるのに必要だった回数は' + count + '回です！');
+
+let i;
+let num = 0;
+
+for(i = 0; i < 11; i ++ ){
+  num = num + i;
+}
+
+alert('1から１０まで足し算した結果は、' + num + 'です！');
