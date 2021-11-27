f8d16cf031365702de2725382f36fc0f108d1ab8
新井峻 <luke1069onega14mas@gmail.com>
2021-11-27 16:30:15 +0900

    JQuery-tabbar

--- jQuery/lesson2/Tabbar-Lesson/js/script.js
+++ jQuery/lesson2/Tabbar-Lesson/js/script.js
@@ -1,7 +1,13 @@
 $(function(){
-  $(".menu-trigger").on("click", function(event){
-    $(this).toggleClass("active");
-    $("#sp-menu").fadeToggle();
+  $('#tab-contents .tab[id != "tab1"]').hide();
+});
+
+$(function(){
+  $("#tab-menu a").on("click", function(event){
+    $("#tab-contents .tab").hide();
+    $("#tab-menu .active").removeClass("active");
+    $(this).addClass("active");
+    $($(this).attr("href")).show();
     event.preventDefault();
   });
 });
